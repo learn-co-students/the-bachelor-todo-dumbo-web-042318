517882242cae14a77de92b412dfb8df1d35e0e4b
teasacura <teasacuraster@gmail.com>
2018-04-14 16:38:34 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  new_arr = data[season]
+  new_arr.each do |hash|
+    hash.each do |description, info|
+      if hash[description] == "Winner"
+        return hash["name"].split[0]
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, all_data|
+    all_data.each do |hash|
+      hash.each do |description, info|
+        if hash[description] == occupation
+          return hash["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, all_data|
+    all_data.each do |hash|
+      hash.each do |description, info|
+        if hash[description] == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, all_data|
+    all_data.each do |hash|
+      hash.each do |description, info|
+        if hash[description] == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  new_arr = data[season]
+  new_arr.each do |hash|
+    hash.each do |description, info|
+      if description == "age"
+        ages << hash[description].to_f
+      end
+    end
+  end
+  average = (ages.reduce(:+)/ages.size).round
 end
